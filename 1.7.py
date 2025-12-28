d2b17d933ef7db97c2ab7009d3ba85913c452e6d
anastasis-tet <Teterina.Nastya@outlook.com>
2025-12-28 22:26:53 +0300

    Task 7

--- 1.7.py
+++ 1.7.py
@@ -9,12 +9,10 @@ root.title("Меню с выходом")
 menubar = tk.Menu(root)
 root.config(menu=menubar)
 
-# Меню File
 file_menu = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Exit", command=exit_app)
 
-# Текст на окне
 label = tk.Label(root, text="Выберите File -> Exit для выхода")
 label.pack(pady=50)
 
